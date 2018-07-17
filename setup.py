780e1be041a1bc641d830474ad084dce4c8de1ec
Dominik Balog <dominicbalog204@gmail.com>
2018-07-17 10:22:34 +0100

    Modified singer and requests for other taps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-setup(name='tap-hubspot',
+setup(name='tap-hubspot-singer',
       version='2.2.2',
       description='Singer.io tap for extracting data from the HubSpot API',
       author='Stitch',
@@ -11,8 +11,8 @@ setup(name='tap-hubspot',
       py_modules=['tap_hubspot'],
       install_requires=[
           'attrs==16.3.0',
-          'singer-python==5.1.1',
-          'requests==2.12.4',
+          'singer-python>=5.1.1',
+          'requests>=2.12.4',
           'backoff==1.3.2',
           'requests_mock==1.3.0',
           'nose'
